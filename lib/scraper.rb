cb04f64b845fb2925b774b63451766f4bcd8d1d9
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-25 20:18:54 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,7 +20,16 @@ class Scraper
   
   def get_courses
   self.get_page.css(".post")
-end
+  end
+  
+  def make_courses
+  self.get_courses.each do |post|
+    course = Course.new
+    course.title = post.css("h2").text
+    course.schedule = post.css(".date").text
+    course.description = post.css("p").text
+  end
+  end
  
 end
 
